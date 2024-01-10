63662246421bcfa8e0766f0ffe014077d09b9e3c
MinhDuc2k03 <149854442+MinhDuc2k03@users.noreply.github.com>
2024-01-10 16:32:07 +0700

    frontend-tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        default:['Montserrat', 'sans-serif'],
+        display: ['Montserrat', 'sans-serif'],
+        body: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
